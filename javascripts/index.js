f98c966665e51264a1b894f9491d070a4823197e
Luis Hernandez <lshrnndz6@gmail.com>
2022-11-26 16:07:32 -0800

    Created Variables for showAssignmentfunction

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,9 @@
 function showAssignment(){
-    
+    const assignmentCollection = document.getElementById("task-collection")
+    const div = document.createElement("div")
+    const header = document.createElement("h3")
+    const subject = document.createElement("li")
+    const titles = document.createElement("li")
+    const date = document.createElement("li")
+    const instruction = document.createElement("li")
 }
