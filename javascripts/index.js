77f3477f19b30f62802b948532c56d2ca519e9ea
Luis Hernandez <lshrnndz6@gmail.com>
2022-11-27 11:14:51 -0800

    Create addAssignment function

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,17 +1,19 @@
+
+
 //GET request for db.json
 function getAssignments(){
     fetch("http://localhost:3000/classAssignments")
     .then(response => response.json())
     .then(data => data.forEach(assignment => showAssignment(assignment)))
 }
+
 //Invoke Function getAssignments to show db.json objects on the DOM
 getAssignments()
 
 
-
 // This function will create a card that will have the information of each homework assignment that is in the db.json.
 //It will also help with creating new assignments added to db.json
-function showAssignment(){
+function showAssignment(assignment){
     const assignmentCollection = document.getElementById("task-collection")
     const div = document.createElement("div")
     div.classList.add("card")
@@ -28,3 +30,10 @@ function showAssignment(){
     div.append(header, subject, titles, date, instruction)
     assignmentCollection.append(div)
 }
+
+//Creating the function addAssigment to create and POST homework assignments.
+function addAssigment(){
+  
+}
+
+
