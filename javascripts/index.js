43558e8e121a238380f5fe76bc17d18e279d3079
Luis Hernandez <lshrnndz6@gmail.com>
2022-11-28 14:53:17 -0800

    Changed event listener and added a function to it

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,17 +3,6 @@ const form = document.querySelector(".add-hw-tasks")
 //Add a 'submit' eventListener to the form variable and pass addAssignment as a parameter
 form.addEventListener('submit', addAssignment)
 
-//GET request for db.json
-function getAssignments(){
-    fetch("http://localhost:3000/classAssignments")
-    .then(response => response.json())
-    .then(data => data.forEach(assignment => showAssignment(assignment)))
-}
-
-//Invoke Function getAssignments to show db.json objects on the DOM
-getAssignments()
-
-
 // This function will create a card that will have the information of each homework assignment that is in the db.json.
 //It will also help with creating new assignments added to db.json. It will also delete created assignments.
 function showAssignment(assignment){
@@ -28,25 +17,32 @@ function showAssignment(assignment){
     const titles = document.createElement("li")
     titles.textContent = `Title: ${assignment.title}`
     const date = document.createElement("li")
-    date.textContent = `Date: ${assignment.dueDate}`
+    date.textContent = `Date due: ${assignment.dueDate}`
     const instruction = document.createElement("li")
     instruction.textContent = `Instructions: ${assignment.instructions}`
     const button = document.createElement("button")
     button.classList.add("delete-btn")
     button.textContent = "Delete Assignment"
     button.id = assignment.id
-    //Event listener to delete card. DELETE request to remove objects from db.json.
+    //Event listener to delete card.
     button.addEventListener('click', () => {
-      fetch(`http://localhost:3000/classAssignments/${assignment.id}`, {
-        method: 'DELETE'
-      })
-      .then(() => div.remove())
+     deleteAssignment(assignment.id) 
     })
-    //Append variables div
+    // //Append variables div
     div.append(header, subject, titles, date, instruction, button)
     //Append div to DOM
     assignmentCollection.append(div)
 }
+
+//GET request for db.json
+function getAssignments(){
+  fetch("http://localhost:3000/classAssignments")
+  .then(response => response.json())
+  .then(data => data.forEach(assignment => showAssignment(assignment)))
+}
+
+//Invoke Function getAssignments to show db.json objects on the DOM
+getAssignments()
 
 //Creating the function addAssigment to create and POST homework assignments.
 function addAssignment(event){
@@ -70,5 +66,18 @@ function addAssignment(event){
       form.reset()
 }
 
-
-
+//Function to delete cards from DOM as well as an alert message for deleting the card.
+function deleteAssignment(ids) {
+  let txt;
+  if (confirm("Are you sure you want to delete this assignment?")) {
+   fetch(`http://localhost:3000/classAssignments/${ids}`, {
+    method: 'DELETE'
+  })
+  .then(() =>{
+  location.reload(false);
+  })
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = "";
+}
